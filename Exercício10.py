3d06b0736f6e047148cd5fc89e346fd7c2783577
Yori <61568561+Yoriih@users.noreply.github.com>
2025-04-20 15:42:21 -0300

    Update Exercício10.py

--- Exercício10.py
+++ Exercício10.py
@@ -2,6 +2,18 @@
 
 # Solicite a idade do usuário
 
-
+idade = int(input("DIGITE A SUA IDADE, E DIREI A SUA FAIXA ETÁRIA: "))
  
 # Classifique-a em "Criança", "Adolescente", "Adulto Jovem" ou "Adulto".
+
+if idade <= 12 :
+    print("Você é uma Criança")
+
+elif 13 <= idade <= 17 :
+    print("Você é um Adolescente")
+
+elif 18 <= idade <= 24 :
+    print("Você é um apenas um Adulto Jovem")
+
+else:
+    print("Você é Adulto")
